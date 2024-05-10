ec2cc9599c99be0adeebfbe3dd36fdb8920fdf67
Jair G <86137431+SrJaizer@users.noreply.github.com>
2024-05-10 19:43:38 -0300

    fix: fix routes name

--- app/page.js
+++ app/page.js
@@ -5,11 +5,11 @@ import Calendar from "./components/Calendar/Calendar";
 export default function Home() {
   return (
     <main>
-      <Link href="/profile" className={styles.userProfileButton}>
+      <Link href="/perfil" className={styles.userProfileButton}>
         Perfil de Usuário
       </Link>
       <Calendar />
-      <Link href="/message" className={styles.dailyMessageButton}>
+      <Link href="/mensagem" className={styles.dailyMessageButton}>
         Mensagem Diária
       </Link>
     </main>
