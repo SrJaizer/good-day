d55724e3a9d4e94f902d433d78d3a2b83d325bbe
Jair G <sr.jaizer@gmail.com>
2024-05-15 20:41:57 -0300

    Fix: fix 'perfil' page

--- app/(pages)/perfil/page.jsx
+++ app/(pages)/perfil/page.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import styles from './Profile.module.css'; // Certifique-se de que o caminho está correto
+import styles from './page.module.css';
 
 export default function Profile() {
   return (
     <main className={styles.profileContainer}>
-      <div className={styles.backButton}>
-        {/* Ícone de seta para voltar, você pode substituir por um ícone de sua escolha ou imagem */}
-        <span className={styles.arrow}>{"<"}</span>
-      </div>
       <div className={styles.avatarContainer}>
-        {/* Substitua 'avatar.png' pelo caminho correto para a imagem do seu perfil */}
-        <img src="avatar.png" alt="Avatar" className={styles.avatar} />
+        <img src="./avatar-example.svg" alt="Avatar" className={styles.avatar} />
+        <h1 className={styles.name}>Nome</h1>
       </div>
-      <h1 className={styles.name}>Nome</h1>
       <div className={styles.statsContainer}>
-        {/* Substitua 'stats-icon.png' pelo caminho correto para a imagem de ícone de estatísticas */}
-        <img src="stats-icon.png" alt="Estatísticas" className={styles.statsIcon} />
+        <img src="stats-icon.svg" alt="Estatísticas" className={styles.statsIcon} />
         <p className={styles.statsText}>Estatísticas</p>
       </div>
     </main>
